c4def05641110e2b2708012d8d02579bee998a89
omar <omarqais225@gmail.com>
2025-10-06 20:12:54 +0300

    fix an issue

--- client/utils/socket.ts
+++ client/utils/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const SOCKET_URL = "http://localhost:5205";
+const SOCKET_URL = process.env.SERVER_URL;
 export const socket = io(SOCKET_URL, {
     withCredentials: true,
 });
