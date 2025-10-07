a31fe4aa7a10cae949b101c5ab46d6fe0f000e29
omar <omarqais225@gmail.com>
2025-10-07 03:33:47 +0300

    fix passport

--- server/config/passport.ts
+++ server/config/passport.ts
@@ -25,7 +25,7 @@ passport.use(
             clientID: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
             callbackURL: process.env.NODE_ENV != "development"
-                ? `${process.env.CLIENT_URL}/api/auth/google/callback`
+                ? `${process.env.SERVER_URL}/api/auth/google/callback`
                 : "http://localhost:5205/api/auth/google/callback",
             scope: ["profile", "email"]
         },
